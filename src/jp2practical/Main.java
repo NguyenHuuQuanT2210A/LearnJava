fc9610aa9eea37ca3fc95151343d3abd86007406
NguyenHuuQuanT2210A <118426160+NguyenHuuQuanT2210A@users.noreply.github.com>
2023-06-22 15:47:07 +0700

    jp2 practical

---
+++ src/jp2practical/Main.java
@@ -0,0 +1,8 @@
+package jp2practical;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerRelationshipManagement crm = new CustomerRelationshipManagement();
+        crm.run();
+    }
+}
